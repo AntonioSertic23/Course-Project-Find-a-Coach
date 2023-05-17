cb6b9b5a26ae739e2fc981776c16c851ca094c04
Antonio Sertić <45394148+AntonioSertic23@users.noreply.github.com>
2023-05-17 18:28:43 +0200

    The "Not Found" Page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>This page could not be found - maybe check out all our</p>
+      <router-link to="/coaches">coaches</router-link>.
+    </base-card>
+  </section>
+</template>
